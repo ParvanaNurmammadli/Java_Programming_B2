ff4ca8012630685ea926acc76274d430c926e803
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-09-16 15:05:56 -0400

    My NEW commit

--- src/day09_a__scanner/Order.java
+++ src/day09_a__scanner/Order.java
@@ -5,9 +5,26 @@ public class Order {
 
 
         Scanner key=new Scanner(System.in);
+        System.out.print("Please, enter a product name:");
+        String productName=key.nextLine();
 
+        System.out.print("Enter a price for a "+ productName+ ":" );
+        double price=key.nextDouble();
 
+        System.out.print("How many"+ productName+ "do you want to buy?");
 
+        int quantity=key.nextInt();
+
+        key.nextLine();
+
+        System.out.print("What should put on your order name:");
+        String orderName=key.nextLine();
+
+        double totalPrice= price*quantity;
+
+        String orderDetails= orderName+ ", your order for " + quantity+ " "+ productName+ " has been placed.\nYour total is $" + totalPrice;
+
+        System.out.println(orderDetails);
 
 
     }
