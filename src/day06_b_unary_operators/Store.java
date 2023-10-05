41b2e4f76c3fb5c6ff4462ceab98409edd008ea7
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-10-04 22:06:13 -0400

    My day16 and changes string commit.

--- src/day06_b_unary_operators/Store.java
+++ src/day06_b_unary_operators/Store.java
@@ -2,13 +2,28 @@ package day_06_b_unary_operators;
 
 public class Store {
 
-    public static void main(String[]args){
+        public static void main(String[] args) {
+
+            String store = "Target";
+            int numOfTv = 100;
 
 
-   String store ="Target";
-   int numOfTv=100;
-   System.out.println("Person comes into the store and they bought a TV");
+            System.out.println("Person comes into the store and they bought a TV");
+            //numOfTv = numOfTv - 1;
+            System.out.println("Number of TV now: " + --numOfTv );
 
+            System.out.println("Another person came and bough 1 more TV");
+            System.out.println("Number of TV now: " + --numOfTv);
+
+            System.out.println("In report, it shows that we have total: " + numOfTv);
+
+            System.out.println("First person has returned a TV");
+            System.out.println("Number of TV now: " + ++numOfTv);
+
+
+
+
+
+        }
 
     }
-}
