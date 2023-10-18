98d4f55f55d62f8b592be0cab49a52a8ee375f86
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-10-18 19:32:12 -0400

    My day 22  array

---
+++ src/day_22array/ReverseSentences.java
@@ -0,0 +1,38 @@
+package day_22array;
+
+public class ReverseSentences {
+
+
+    /*
+        today is java
+
+        java is today
+ */
+    public class ReverseSentence {
+        public static void main(String[] args) {
+
+            String sentence  = "today is java";
+
+            // I need get each word and concatenate it starting from end.
+            String [] words = sentence.split(" ");  //[today, is, java]
+            System.out.println(words.length); //
+
+
+            for (int i = 0; i < words.length; i++) {
+                System.out.println(words[i]);
+            }
+
+            System.out.println("--------------------");
+            String result = "";
+            for (int i = words.length-1; i >=0 ; i--) {
+
+                result += words[i] + " ";
+                //System.out.println(words[i]);
+
+            }
+
+            System.out.println(result.trim());
+
+        }
+    }
+}
