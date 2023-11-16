32cd55b09b5dc0c6ec1aa26afb52659ad7f0b68b
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-11-15 23:21:22 -0500

    My day 34 inherits

--- src/day02_print_statements/HelloWorld.java
+++ src/day02_print_statements/HelloWorld.java
@@ -1,9 +1,28 @@
 package day02_print_statements;
 
-public class HelloWorld
-{
 
-     public static void main (String [] args){
-             System.out.println("Hello World");
-     }
-}
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HelloWorld {
+
+
+    /*
+        Create a class called EscapeSequence2
+        Create a main method
+        Output this exactly:
+
+            "iphone", "laptop", "xbox", "webcam", "monitor"
+      */
+
+     public static void main(String[] args) {
+          ArrayList<Integer> numsOne = new ArrayList<>();
+          numsOne.addAll( Arrays.asList( 4,1,8,-42,2,10));
+          
+
+
+     }}
+
+
+
+
