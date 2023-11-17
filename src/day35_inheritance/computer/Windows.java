8c94482c18192664516e9f5a4b299cacf2273f02
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-11-16 22:12:11 -0500

    oop, encapsulation, super

---
+++ src/day35_inheritance/computer/Windows.java
@@ -0,0 +1,10 @@
+package day35_inheritance.computer;
+
+public class Windows extends Computer{
+
+    public Windows (int memory) {
+    super("Windows",  memory);
+}}
+
+
+
---
+++ src/day35_inheritance/multi_level_inheritance/D.java
@@ -0,0 +1,5 @@
+package day35_inheritance.multi_level_inheritance;
+
+public class D {
+
+}
