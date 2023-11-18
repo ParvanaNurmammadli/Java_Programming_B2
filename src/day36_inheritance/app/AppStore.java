8d0ce1e82989c0536a4f792c23dd1d4e14dc7b5c
ParvanaNurmammadli <parvana.nurmammadli@gmail.com>
2023-11-18 15:03:42 -0500

    objects class, protectded access modifier, final ketword, method overviding

---
+++ src/day36_inheritance/app/AppStore.java
@@ -0,0 +1,22 @@
+package day36_inheritance.app;
+
+public class AppStore {
+    //It is a RUNNER class meaning I am running my code from this class
+    public static void main(String[] args) {
+
+        Instagram objIns = new Instagram(4.0);
+        objIns.download();
+        objIns.follow();
+
+
+        System.out.println();
+        YouTube objYou = new YouTube( 3.4);
+        objYou.download();
+        objYou.subscribe();
+
+    }
+
+
+
+}
+
